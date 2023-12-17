a60b596eae4cca3f6cc7f683f4b2f4eb2966e4ee
xmzs <1195566313@qq.com>
2023-12-17 11:29:26 +0800

    husky

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <h1>refgdgfdgfdgdgd</h1>
+    <h1>husky</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
